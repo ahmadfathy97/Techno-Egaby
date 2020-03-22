b00387c9984eb3fbc925d6181ba3b37853feb20c
ahmadfathy97 <af1381@fayoum.edu.eg>
2020-03-22 03:30:10 +0200

    fixing iframe pause issue bu using player_api

--- adminpanel/video/index.php
+++ adminpanel/video/index.php
@@ -1,50 +1,99 @@
-<?php include '../components/header.php'; ?>
-</head>
+<?php 
+
+session_start();
+if (isset($_SESSION['user'])) {
+  include '../components/header.php'; 
+  include '../../connDB.php'; 
+  if (isset($_GET['id'])&& is_numeric($_GET['id'])) {
+    $id_video = $_GET['id'];
+    if (isset($_GET['accp'])) {
+      $update =$conn->prepare("UPDATE video SET accept= 1  WHERE id='$id_video'") ;
+      $update->execute();
+      header('location:../index.php');
+      
+    }
+    if (isset($_GET['unaccp'])) {
+      $update =$conn->prepare("UPDATE video SET accept= 0  WHERE id='$id_video'") ;
+      $update->execute();
+      header('location:../index.php');
+      
+    }
+    $Allvideo=$conn->prepare("SELECT * FROM video WHERE id = '$id_video' ");
+    $Allvideo->execute();
+    $video=$Allvideo->fetch();
+    $count =  $Allvideo->rowCount();
+    if ($count != 1) {
+      echo 'error :(';
+    }else{
+      ?>
+      </head>
 <body>
-  <?php include '../components/nav.php' ?>
-  <div class="container">
+  <?php 
+  include '../components/nav.php';
+   parse_str(parse_url($video['vid_url'],PHP_URL_QUERY),$arr);
+  echo ' 
+    <div class="container">
 
-    <div class="row mt-5">
+      <div class="row mt-5">
 
-      <div class="col-md-12">
-        <div class="video m-1 p-3 border shadow">
-          <h2 class="video-title">عنوان الفيديو</h2>
-          <p class="small-desc">وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو وصف الفيديو </p>
-          <h3 class="">صاحب الفيديو</h3>
-          <p class=""> معلومات الاتصال معلومات الاتصال معلومات الاتصال معلومات الاتصال معلومات الاتصال</p>
-          <iframe src="https://www.youtube.com/embed/UHp2sJLscGk" class="m-1" border-0 width="100%" style="height:60vh" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <div class="buttons">
-            <a href="#" class="btn btn-danger float-right" role="button" data-toggle="modal" data-target="#exampleModal">حذف</a>
-            <a href="/adminpanel/edit-video/" class="btn btn-info" role="button">تعديل</a>
-            <a href="#" class="btn btn-success" role="button">قبول</a>
-            <a href="#" class="btn btn-warning" role="button">تعليق النشر</a>
+        <div class="col-md-12">
+          <div class="video m-1 p-3 border shadow">
+            <h2 class="video-title">'.$video['vid_title'].'</h2>
+            <p class="small-desc">'.$video['vid_desc'].'</p>
+            <h3 class="">'.$video['name'].'</h3>
+            <p class="">'.$video['call_me'].'</p>
+            <iframe src="https://www.youtube.com/embed/'.$arr['v'].'" class="m-1" border-0 width="100%" style="height:60vh" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="buttons">
+              <a href="#" class="btn btn-danger float-right" role="button" data-toggle="modal" data-target="#exampleModal">حذف</a>
+              <a href="/adminpanel/edit-video/?id='.$video['id'].'" class="btn btn-info" role="button">تعديل</a>';
+              if ($video['accept']==0) {
+                echo '<a href="?accp&id='.$video['id'].'" class="btn btn-success" role="button">قبول</a>';
+              }else{
+                echo '<a href="?unaccp&id='.$video['id'].'" class="btn btn-warning" role="button">تعليق النشر</a>';
+              }
+
+
+              
+      echo'   </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content" >
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">تأكيد</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <div class="text-danger">
-            هل انت متأكد من حذف هذا الفيديو؟؟
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content" >
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">تأكيد</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="text-danger">
+              هل انت متأكد من حذف هذا الفيديو؟؟
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">إغلاق</button>
+            <a href="?del&id='.$video['id'].'" type="button" class="btn btn-danger" data-dismiss="modal">حذف</a>
           </div>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">إغلاق</button>
-          <a href="#" type="button" class="btn btn-danger" data-dismiss="modal">حذف</a>
-        </div>
       </div>
-    </div>
-  </div>
+    </div>';
+
+    }
+  }else{
+    echo 'error :(';
+  }
+  
+}else{
+  header('location:login.php');
+}
+
+?>
+
   <?php include '../components/footer.php'; ?>
 </body>
 </html>
+
--- public/script/home.js
+++ public/script/home.js
@@ -17,3 +17,29 @@ function write(){
   }, 50);
 
 }
+
+
+var player;
+
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('home-video', {
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    document.getElementById('exampleModal').onblur = function () {
+      player.pauseVideo();
+    }
+    var pauseButton1 = document.getElementById("close-vid-1");
+    var pauseButton2 = document.getElementById("close-vid-2");
+
+    pauseButton1.addEventListener("click", function() {
+        player.pauseVideo();
+    });
+    pauseButton2.addEventListener("click", function() {
+        player.pauseVideo();
+    });
+}
